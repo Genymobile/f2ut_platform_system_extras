b46db699e25f85b1c1a471fa3ad3882ae4d1d18a
Dirk Vogt <dirk@fairphone.com>
2016-01-19 16:08:00 +0100

    FP2-1752: add SU for apps to userdebug builds.
    Do not build standard su binary when FP2_USE_APPOPS_SU is set.
    
    Change-Id: Iec4883b4de183161a1e147ec1cf2d233274255e6

--- su/Android.mk
+++ su/Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(FP2_USE_APPOPS_SU),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -13,3 +14,4 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := debug
 
 include $(BUILD_EXECUTABLE)
+endif
